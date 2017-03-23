ea02faa12740df08242286dbefb439867b2b00cf
Herminio García González <herminiogg@hotmail.com>
2017-03-23 12:53:49 +0100

    Changed some pages to perform load tests

--- src/main/java/es/uniovi/asw/controller/Main.java
+++ src/main/java/es/uniovi/asw/controller/Main.java
@@ -7,6 +7,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 
 @Controller
 public class Main {
@@ -15,8 +20,21 @@ public class Main {
 
   @RequestMapping("/")
   public ModelAndView landing(Model model) {
-    LOG.info("Landing page access");
     return new ModelAndView("landing");
+  }
+
+  @RequestMapping("/sort")
+  public ModelAndView sort(Model model) {
+    Random random = new Random();
+    LOG.info("Landing page access");
+    long millis0 = System.currentTimeMillis();
+    IntStream.range(1, 1000).forEach(i -> {
+      IntStream randoms = IntStream.range(1, 100000).map(j -> random.nextInt());
+      Arrays.asList(randoms.sorted().toArray());
+    });
+    long millis1 = System.currentTimeMillis();
+    LOG.info("Elapsed: " + (millis1 - millis0));
+    return new ModelAndView("sort");
   }
   
 }
